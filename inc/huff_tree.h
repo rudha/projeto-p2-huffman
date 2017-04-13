1ef5d5b5a16a2a734166f20dc805f2201599b238
JCSaint <jeancsantoscdc@gmail.com>
2017-04-13 19:30:11 -0300

    adding a pointer

--- inc/huff_tree.h
+++ inc/huff_tree.h
@@ -10,10 +10,11 @@ typedef struct huff_tree node;
 struct huff_tree {
 	int frequency;
 	char character;
+	node *next 
 	node *left;
 	node *right;
 };
 
-node* create_huff_tree(int frequency, char character, node *left, node *right);
+node* create_huff_tree(int frequency, char character, node *left, node *right, node *next);
 
 #endif /* HUFF_TREE_H */
